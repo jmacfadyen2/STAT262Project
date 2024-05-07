20a179482b3e13de0bd76980d0844a6f192bcdba
jesusrod44 <jrodriguez44@huskers.unl.edu>
2024-05-06 19:07:24 -0500

    added the mean to the histogram

--- tuition.R
+++ tuition.R
@@ -18,6 +18,8 @@ ggplot(pri_tution_cost, aes(out_of_state_total))+geom_histogram()
 #Expected Value
 Ex<-mean(pri_tution_cost$out_of_state_total)
 
+Mx<-min(pri_tution_cost$out_of_state_total)
+
 #Bootstrap
 
 #Set up an empty data set with 2 columns: simulation number, bootstrap mean#
@@ -32,13 +34,30 @@ for(i in 1:10000){
   boot.samples$min_tuition[i]<-min(sample(pri_tution_cost$out_of_state_total,size=30,replace=TRUE))
 }
 
-
-
 head(boot.samples)
 
+mean_min_tuition<-mean(boot.samples$min_tuition)
+
 #Histogram#
-boot.hist<-ggplot(boot.samples, aes(min_tuition)) + geom_histogram(binwidth=1000)+ geom_vline(xintercept = Ex, color = "red", linetype = "dashed")
+boot.hist<-ggplot(boot.samples, aes(min_tuition)) + geom_histogram(binwidth=1000)+ geom_vline(xintercept = mean_min_tuition, color = "red", linetype = "dashed")
 
 #See the plot#
 boot.hist
+#What does our histogram tell us??
+
+
+# Load required library
+library(ggplot2)
+
+# Generate random numbers from exponential distribution
+data <- data.frame(x = rexp(1000, rate = 1/(Ex/10000)) ) # Change the rate parameter as needed
+
+# Plot the density of the generated data using ggplot2
+ggplot(data, aes(x)) +
+  geom_density(fill = "blue", alpha = 0.5) +
+  theme_minimal() +
+  labs(x = "Values", y = "Density", title = "Exponential Distribution")
+
+
+
 
